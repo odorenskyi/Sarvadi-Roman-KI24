db1d32b08dc01e68486214d3ccb990e0cd25371c
Сарваді Роман <genadiygenialen@gmail.com>
2025-06-15 14:05:41 +0300

    виправив  помилку в коді

--- lab12/prj/Teacher/Teacher/Teacher.cpp
+++ lab12/prj/Teacher/Teacher/Teacher.cpp
@@ -4,33 +4,35 @@
 #include <sstream>
 #include <cstring>
 #include <cmath>
-#include "ModulesSarvadi.h"
+#include "../ModulesSarvadi.h"
 
 using namespace std;
 
 struct TestSuite {
-    float action = 0;
+    float radius = 0;
+    float height = 0;
     float expResult = 0;
 };
 
-vector<TestSuite> readCSVFile(string filePath) {
+vector<TestSuite> readCSVFiles(string filePath) {
     ifstream testSuite(filePath);
-    vector<string> row;
     vector<TestSuite> autotest;
     TestSuite ts;
     string line, value;
+
     getline(testSuite, line); // Пропускаємо заголовок
 
     while (getline(testSuite, line)) {
-        row.clear();
         stringstream s(line);
+        vector<string> row;
 
         while (getline(s, value, ',')) {
             row.push_back(value);
         }
 
-        ts.action = stof(row[0]);
-        ts.expResult = stof(row[1]);
+        ts.radius = stof(row[0]);
+        ts.height = stof(row[1]);
+        ts.expResult = stof(row[2]);
 
         autotest.push_back(ts);
     }
@@ -47,28 +49,30 @@ int main() {
     if (found == string::npos) {
         testResult.open("TestResult.txt");
         if (!testResult.is_open()) return -1;
+
         for (int i = 0; i < 100; i++) {
             cout << '\a';
         }
+
         testResult << "Встановлені вимоги порядку виконання лабораторної роботи порушено!";
     }
     else {
         testResult.open("..\\..\\TestSuite\\TestResult.txt");
         if (!testResult.is_open()) return -1;
 
-        ClassLab12_Sarvadi sphere(0);
-        vector<TestSuite> autotest = readCSVFile("..\\..\\TestSuite\\TS.txt");
+        vector<TestSuite> autotest = readCSVFiles("..\\..\\TestSuite\\TS.txt");
 
         for (int i = 0; i < autotest.size(); i++) {
-            sphere.setRadius(autotest[i].action);
-            if (round(autotest[i].expResult) == round(sphere.getAreaSphere())) {
+            ClassLab12_Sarvadi cone(autotest[i].radius, autotest[i].height);
+
+            if (round(autotest[i].expResult) == round(cone.getVolumeCone())) {
                 testResult << "test №" << i + 1 << " -> passed" << endl << endl;
             }
             else {
                 testResult << "test №" << i + 1 << " -> failed" << endl
                     << "answers don't match" << endl
                     << "It should be -> " << autotest[i].expResult << endl
-                    << "Received response -> " << sphere.getAreaSphere() << endl << endl;
+                    << "Received response -> " << cone.getVolumeCone() << endl << endl;
             }
         }
     }
