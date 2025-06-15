3b33b421591428c2c8128129650e514e42c18192
Сарваді Роман <genadiygenialen@gmail.com>
2025-06-15 14:03:55 +0300

    змінив файл(додав height а також змінив формулу)

--- lab12/prj/Teacher/Teacher/ModulesSarvadi.h
+++ lab12/prj/Teacher/Teacher/ModulesSarvadi.h
@@ -1,37 +1,52 @@
 #ifndef MODULESSARVADI_H_INCLUDED
 #define MODULESSARVADI_H_INCLUDED
+
+#include <cmath>
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-#include <cmath>
-
 class ClassLab12_Sarvadi {
 public:
     ClassLab12_Sarvadi() {}
-    ClassLab12_Sarvadi(float radius);
+    ClassLab12_Sarvadi(float radius, float height);
+
     void setRadius(float radius);
-    float getRadius();
-    float getAreaSphere();
+    void setHeight(float height);
+
+    float getRadius() const;
+    float getHeight() const;
+
+    float getVolumeCone() const;
+
 private:
     float radius = 0.0;
+    float height = 0.0;
 };
 
-ClassLab12_Sarvadi::ClassLab12_Sarvadi(float radius) {
+ClassLab12_Sarvadi::ClassLab12_Sarvadi(float radius, float height) {
     this->radius = radius;
+    this->height = height;
 }
 
 void ClassLab12_Sarvadi::setRadius(float radius) {
     this->radius = radius;
 }
 
-float ClassLab12_Sarvadi::getRadius() {
+void ClassLab12_Sarvadi::setHeight(float height) {
+    this->height = height;
+}
+
+float ClassLab12_Sarvadi::getRadius() const {
     return radius;
 }
 
-float ClassLab12_Sarvadi::getAreaSphere() {
-    return 4 * M_PI * pow(radius, 2);
+float ClassLab12_Sarvadi::getHeight() const {
+    return height;
+}
+
+float ClassLab12_Sarvadi::getVolumeCone() const {
+    return (M_PI * radius * radius * height) / 3.0;
 }
 
 #endif // MODULESSARVADI_H_INCLUDED
-#pragma once
